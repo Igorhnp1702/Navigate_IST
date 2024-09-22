a4018fd153a63de0c6ad32bd44db91878a979424
Igorhnp1702 <igorhnp1702@gmail.com>
2024-09-22 02:29:13 +0100

    Adição dos ficheiros de teste. Basta correr "make rundiffs" no terminal, na diretoria "Fase1", para testar todos os ficheiros de teste disponíveis até agora

--- Fase1/prob_utils.h
+++ Fase1/prob_utils.h
@@ -43,10 +43,9 @@ int read_problem(Files *fblock, ProbInfo **prob);
  * 
  * Retorno: Void
  */
+void bad_prob_ans(FILE *fpOut, ProbInfo **prob_node);
 
 void Get_tarefa(ProbInfo **prob, Files *fblock, int L, int C, int l_2, int c_2, int prob_flag);
-
-void bad_prob_ans(FILE *fpOut, ProbInfo **prob_node);
 
 /*
  * t1_solver()
